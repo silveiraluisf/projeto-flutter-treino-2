b8dade2e20ab62113cd40563ec9bfeda54893547
silveiraluisf <silveiraluis88@gmail.com>
2022-08-19 17:11:54 -0300

    refatoração do cod

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
@@ -5,12 +6,8 @@ Future<Database> getDatabase() async {
   final String path = join(await getDatabasesPath(), 'bytebank.db');
   return openDatabase(path,
     onCreate: (db, version) {
-      db.execute('CREATE TABLE contacts('
-          'id INTEGER PRIMARY KEY, '
-          'name TEXT, '
-          'account_number INTEGER)');
+      db.execute(ContactDao.tableSql);
     },
     version: 1,
-    //    onDowngrade: onDatabaseDowngradeDelete,
   );
 }
