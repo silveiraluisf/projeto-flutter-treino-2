a71f3d1bed558f7eb29055b2e13b696aa881df2c
silveiraluisf <silveiraluis88@gmail.com>
2022-08-19 11:25:46 -0300

    lista de contatos adiciona novos itens

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,30 +1,28 @@
 import 'package:bytebank/screens/contacts_form.dart';
 import 'package:flutter/material.dart';
 
+import '../models/contact.dart';
+
 class ContactsList extends StatelessWidget {
-  const ContactsList({Key? key}) : super(key: key);
+
+  final List<Contact> contacts = [];
 
   @override
   Widget build(BuildContext context) {
+    contacts.add(Contact(0, 'Lolo', 2000));
+    contacts.add(Contact(0, 'Lolo', 2000));
+    contacts.add(Contact(0, 'Lolo', 2000));
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contacts'),
       ),
-      body: ListView(
-        children: const <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Luís',
-                style: TextStyle(fontSize: 20.0),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contact
+          contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -35,6 +33,27 @@ class ContactsList extends StatelessWidget {
           ).then((newContact) => debugPrint(newContact.toString()));
         },
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+
+  final Contact contact;
+  _ContactItem(this.contact);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: const TextStyle(fontSize: 20.0),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: const TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
