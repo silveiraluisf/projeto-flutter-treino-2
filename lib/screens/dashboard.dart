4c7e127f3151624ca72a1bac6c5c6a18df860e2b
silveiraluisf <silveiraluis88@gmail.com>
2022-08-22 11:52:09 -0300

    Adicionado botoes transfer e transaction list. adicionado a página de transaction e algumas refatorações de cod

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -18,46 +18,72 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    (MaterialPageRoute(
-                      builder: (context) => const ContactsList(),
-                    )),
-                  );
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const <Widget>[
-                      Icon(
-                        Icons.people,
-                        color: Colors.white,
-                        size: 24.0,
-                      ),
-                      Text(
-                        'Contacts',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          )
+          SizedBox(
+            height: 120,
+            child:
+                ListView(scrollDirection: Axis.horizontal, children: <Widget>[
+              _FeaturesItem('Transfer', Icons.monetization_on, onClick: () {
+                _showContactList(context);
+              }),
+              _FeaturesItem('Transaction Feed', Icons.description, onClick: () {
+                print('transaction feed was clicked');
+              }),
+            ]),
+          ),
         ],
       ),
     );
   }
+}
+
+class _FeaturesItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  const _FeaturesItem(this.name, this.icon, {required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void _showContactList(BuildContext context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => const ContactsList(),
+    ),
+  );
 }
